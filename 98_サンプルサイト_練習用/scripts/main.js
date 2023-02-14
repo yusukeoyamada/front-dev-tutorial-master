a2079f1c3bbe9c917ce73d5da646f4930a523926
yusukeoyamada <fa19940118@gmail.com>
2023-02-14 09:18:16 +0900

    * ガチで学びたい人のための WEB 開発実践入門（フロントエンド編）教材対応 * セッション 9: 「本格的な web サイトの作成」の以下部分対応   * Header セクション部分   * Footer セクション部分

--- 98_サンプルサイト_練習用/scripts/main.js
+++ 98_サンプルサイト_練習用/scripts/main.js
@@ -1,5 +1,6 @@
 // ここにメインのjsの記述を書いていく。
 document.addEventListener('DOMContentLoaded', function () {
+  // 「ヒーロースライダー」部分で使用
   // swiperを起動する処理
   const hero = new HeroSlider('.swiper');
   hero.start();
@@ -8,6 +9,8 @@ document.addEventListener('DOMContentLoaded', function () {
   // 以下は試験的に記述(「Find Your House」部分だけ動く)
   // const ta = new TweenTextAnimation('.tween-animate-title');
   // ta.animate();
+
+  // 「Houseセクション(Find Your House)」部分で使用
   // 「Find Your House」部分で使用
   // 以下は、「05_JavaScriptを実践で使う/030_スクロール監視クラスの作成/end/main.js」から取ってきている。
   const _textAnimation = function (el, inview) {
@@ -18,6 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
   }
   const so = new ScrollObserver('.tween-animate-title', _textAnimation);
 
+  // 「Houseセクション(Find Your House)」部分で使用
   // 「家の画像と、その家の名前」部分で使用
   const _inviewAnimation = function (el, inview) {
     // 画面内にpointer
@@ -25,8 +29,30 @@ document.addEventListener('DOMContentLoaded', function () {
       el.classList.add('inview');
     // 画面外にpointer
     } else {
-      el.classList.add('remove');
+      el.classList.remove('inview');
     }
   }
   const so2 = new ScrollObserver('.cover-slide', _inviewAnimation);
+
+  // 「ヘッダー」部分で使用
+  // 「nav-trigger」関連で使用
+  const header = document.querySelector('.header');
+  const _navAnimation = function (el, inview) {
+    // 画面内にpointer
+    if(inview) {
+      // ここでは、画面内にいるときは、「triggered」を無くしたい
+      header.classList.remove('triggered');
+    // 画面外にpointer
+    } else {
+      // ここでは、画面外にいるときは、「triggered」を付けたい
+      header.classList.add('triggered');
+    }
+  }
+  // 「{ once: false }」をつけると、監視が切れない状態になる
+  // 入れないと、「inview = true」の時で、監視が切れてしまう
+  const so3 = new ScrollObserver('.nav-trigger', _navAnimation, { once: false });
+
+  // 「ヘッダー」部分で使用
+  // 「mobile-menu__btn」を起動させる為に
+  new MobileMenu();
 });
